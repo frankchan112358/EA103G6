e94cc5d6305c60983c3bbf01ce9339d56f11a6c3
chen55680 <yymm55680@gmail.com>
2020-10-08 22:43:12 +0800

    修改VO讓其完成三要素

--- src/com/emp/model/EmpVO.java
+++ src/com/emp/model/EmpVO.java
@@ -1,33 +1,46 @@
 package com.emp.model;
 
-public class EmpVO implements java.io.Serializable{
+public class EmpVO implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private String empNo;
 	private String userNo;
 	private Integer empStatus;
 	private String empName;
-	
-	
+
+	public EmpVO() {
+
+	}
+
 	public String getEmpNo() {
 		return empNo;
 	}
+
 	public void setEmpNo(String empNo) {
 		this.empNo = empNo;
 	}
+
 	public String getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(String userNo) {
 		this.userNo = userNo;
 	}
+
 	public Integer getEmpStatus() {
 		return empStatus;
 	}
+
 	public void setEmpStatus(Integer empStatus) {
 		this.empStatus = empStatus;
 	}
+
 	public String getEmpName() {
 		return empName;
 	}
+
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
