82735c145b30f322bcae2cded1a829089a9061c3
chen55680 <yymm55680@gmail.com>
2020-10-08 22:43:46 +0800

    修改VO讓其完成三要素

--- src/com/notify/model/NotifyVO.java
+++ src/com/notify/model/NotifyVO.java
@@ -2,28 +2,38 @@ package com.notify.model;
 
 import java.sql.Timestamp;
 
-public class NotifyVO implements java.io.Serializable{
-	
+public class NotifyVO implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private String userNo;
 	private String notifyContent;
 	private Timestamp notifyDate;
-	
-	
+
+	public NotifyVO() {
+
+	}
+
 	public String getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(String userNo) {
 		this.userNo = userNo;
 	}
+
 	public String getNotifyContent() {
 		return notifyContent;
 	}
+
 	public void setNotifyContent(String notifyContent) {
 		this.notifyContent = notifyContent;
 	}
+
 	public Timestamp getNotifyDate() {
 		return notifyDate;
 	}
+
 	public void setNotifyDate(Timestamp notifyDate) {
 		this.notifyDate = notifyDate;
 	}
