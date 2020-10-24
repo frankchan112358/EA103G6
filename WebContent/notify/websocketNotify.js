fb26762284290342f0717b9297914cba599769e4
yymm5 <yymm5@LAPTOP-M0JM1LR7>
2020-10-24 11:55:49 +0800

    修改時間問題及簡化處理

--- WebContent/notify/websocketNotify.js
+++ WebContent/notify/websocketNotify.js
@@ -34,8 +34,11 @@
 					
 						if(dateForLong <= (1000*60*60)){ //時間短於一小時
 							showNotifyDay=new Date(dateForLong).getMinutes();							
-							if(showNotifyDay<3){showNotifyDay="剛剛"}	
-							showNotifyDay=showNotifyDay+"分鐘前";
+							if(showNotifyDay<3){
+								showNotifyDay="剛剛"
+							}else{
+								showNotifyDay=showNotifyDay+"分鐘前";
+							}		
 							
 						}else if(dateForLong <= (1000*60*60*24)){ //時間短於一天
 							
@@ -50,7 +53,7 @@
 						}else if(dateForLong <= (1000*60*60*24*30)){ //時間短於三十天
 							
 							var minusTimeZoneDate=dateForLong+(timeZone*60*1000);
-							showNotifyDay=new Date(minusTimeZoneDate).getDate()+"天前";
+							showNotifyDay=new Date(minusTimeZoneDate).getDate()-1+"天前";
 							
 						}else{ //時間長於一個月
 							showNotifyDay="一個月前";
@@ -84,40 +87,9 @@
 					var dateForNow=new Date().getTime();				
 					var notifyTitle=jsonObj.title;
 					var notifyContent=jsonObj.content;
-					var countTimeForShow=jsonObj.time;
-					
-					//進行時間處理，僅適用於 GMT+的地方
-					var dateForLong=dateForNow-countTimeForShow;
-					var timeZone=new Date().getTimezoneOffset();
-					var showNotifyDay;
-				
-					if(dateForLong <= (1000*60*60)){ //時間短於一小時
-						showNotifyDay=new Date(dateForLong).getMinutes();							
-						if(showNotifyDay<3){showNotifyDay="剛剛"}	
-						showNotifyDay=showNotifyDay+"分鐘前";
-						
-					}else if(dateForLong <= (1000*60*60*24)){ //時間短於一天
-						
-						if(new Date(dateForLong).getDate()==2){
-							showNotifyDay=new Date(dateForLong).getHours()+24+(timeZone/60)+"小時前";	//取得小時會自動時區換算成當地時間顧會發生錯誤							
-						}else {
-							showNotifyDay=new Date(dateForLong).getHours();							
-							showNotifyDay=showNotifyDay+(timeZone/60);     //小時要忽略時區故要計算
-							showNotifyDay=showNotifyDay+"小時前";
-						}
-						
-					}else if(dateForLong <= (1000*60*60*24*30)){ //時間短於三十天
-						
-						var minusTimeZoneDate=dateForLong+(timeZone*60*1000);
-						showNotifyDay=new Date(minusTimeZoneDate).getDate()+"天前";
-						
-					}else{ //時間長於一個月
-						showNotifyDay="一個月前";
-					}
-					
-					//時間處理結束，僅適用於 GMT+的地方
-					
-					
+					var countTimeForShow="剛剛";
+										
+										
 					$("#decorateForNotification").prepend(`<li class="unread">
                             <div class="d-flex align-items-center show-child-on-hover">
                             <span class="d-flex flex-column flex-1">
@@ -125,7 +97,7 @@
                                 <span class="msg-a fs-sm">
                                   ${notifyContent}
                                 </span>
-                                <span class="fs-nano text-muted mt-1">${showNotifyDay}</span>
+                                <span class="fs-nano text-muted mt-1">${countTimeForShow}</span>
                             </span>
                             <div class="show-on-hover-parent position-absolute pos-right pos-bottom p-3">
                                 <a href="#" class="text-muted" title="delete"><i class="fal fa-trash-alt"></i></a>
