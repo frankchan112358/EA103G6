a879cbf793c6358f875947c8217327648f518518
Ivy <Ivy@DESKTOP-TURP3JI>
2020-10-26 16:01:15 +0800

    刪測試code

--- src/com/teachingfile/controller/DownloadFile.java
+++ src/com/teachingfile/controller/DownloadFile.java
@@ -1,12 +1,8 @@
 package com.teachingfile.controller;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,9 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
-import com.course.model.CourseVO;
 import com.teachingfile.model.TeachingFileService;
 import com.teachingfile.model.TeachingFileVO;
 
@@ -30,25 +24,18 @@ public class DownloadFile extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		req.setCharacterEncoding("UTF-8");
-		System.out.println("downloadFile : line 30");
 
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 
-			System.out.println("downloadFile : line 35");
 			try {
-				
-				System.out.println("downloadFile : line 38");
 				//傳檔案名稱進來
 //				String courseNo = req.getParameter("courseNo");
 				String queryString = req.getQueryString();
-				System.out.println("queryString :" + queryString);
-				
-				System.out.println("downloadFile : line 44");
+
 				TeachingFileService teachingFileSvc = new TeachingFileService();
 				 TeachingFileVO teachingFileVO= teachingFileSvc.getOneTeachingFile(queryString);
 				
-				 System.out.println("downloadFile : line 48");
 				//建立資料夾
 				String path = req.getSession().getServletContext().getRealPath(queryString + ".pdf");
 				File file = new File(path);
@@ -56,19 +43,14 @@ public class DownloadFile extends HttpServlet {
 				if (!file.exists())
 					file.mkdirs();
 				
-				System.out.println("downloadFile : line 56");				
 				//用VO.File取得檔案(byte[])接outputStream
-				System.out.println(teachingFileVO.getTeachingFile());
-				System.out.println(teachingFileVO.getTeachingFileNo());
-				InputStream is = new BufferedInputStream(new ByteArrayInputStream(teachingFileVO.getTeachingFile()));
-				FileOutputStream fos = new FileOutputStream(queryString + ".pdf");
-				byte[] buffer = new byte[is.available()];
-				is.read(buffer);
-				fos.write(buffer);
-				fos.close();
-				is.close();
+				ByteArrayInputStream bis = new ByteArrayInputStream(teachingFileVO.getTeachingFile());
+				ServletOutputStream sos = res.getOutputStream();
+				byte[] loadFile = new byte[bis.available()];
+				bis.read(loadFile);
+				sos.write(loadFile);
+				sos.close();
 				
-				System.out.println("downloadFile : line 66");
 				//回到原本畫面(嗎? 
 				String url = "/back-end/teachingFile/listAllTeachingFile2.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url);
