e874f27f3dfee35bb7d147a6ee3377b39fe88f7a
Ivy <Ivy@DESKTOP-TURP3JI>
2020-10-30 20:56:37 +0800

    keke換電腦繼續 XD

---
+++ src/com/finalscore/controller/FinalScoreAjax.java
@@ -0,0 +1,27 @@
+package com.finalscore.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class FinalScoreAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		res.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		if("update".equals(action)) {
+			
+			
+		}
+	}
+
+}
